7495dd8ae95f5a25043b5d1adf2c419d65cbbe10
Jayacha7 <113333328+Jayacha7@users.noreply.github.com>
2023-07-18 17:30:53 +0530

    yes

--- app.py
+++ app.py
@@ -17,23 +17,15 @@ def predict_caption(model, image, tokenizer, max_length):
     in_text = 'startseq'
     for i in range(max_length):
         sequence = tokenizer.texts_to_sequences([in_text])[0]
-        # pad the sequence
         sequence = pad_sequences([sequence], max_length)
-        # predict next word
         yhat = model.predict([image, sequence], verbose=0)
-        # get index with high probability
         yhat = np.argmax(yhat)
-        # convert index to word
         word = idx_to_word(yhat, tokenizer)
-        # stop if word not found
         if word is None:
             break
-        # append word as input for generating next word
         in_text += " " + word
-        # stop if we reach end tag
         if word == 'endseq':
             break
-      
     return in_text
 
 def idx_to_word(integer, tokenizer):
@@ -58,5 +50,5 @@ if uploaded_file is not None:
     with open(os.path.join("", 'allcap.pkl'), 'rb') as f:
         all_captions = pickle.load(f)
     tokenizer.fit_on_texts(all_captions)
-
-    st.write(predict_caption(model, feature, tokenizer, 35))
+    sqw =predict_caption(model, feature, tokenizer, 35)
+    st.write(sqw.replace("startseq","").replace("endseq",""))
